b3de7ee51a9c61270d3424d2249db10b9886efb4
Nikita B <Omkwood@mail.ru>
2024-04-12 23:37:50 +0300

    Исправление ошибок

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnAdd_Click = new Button();
+            btnDelete_Click = new Button();
+            dataGridView1 = new DataGridView();
+            titleBox = new TextBox();
+            authorBox = new TextBox();
+            yearBox = new TextBox();
+            genreBox = new TextBox();
+            clientBox = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAdd_Click
+            // 
+            btnAdd_Click.Location = new Point(74, 198);
+            btnAdd_Click.Name = "btnAdd_Click";
+            btnAdd_Click.Size = new Size(75, 23);
+            btnAdd_Click.TabIndex = 0;
+            btnAdd_Click.Text = "button1";
+            btnAdd_Click.UseVisualStyleBackColor = true;
+            btnAdd_Click.Click += button1_Click;
+            // 
+            // btnDelete_Click
+            // 
+            btnDelete_Click.Location = new Point(58, 255);
+            btnDelete_Click.Name = "btnDelete_Click";
+            btnDelete_Click.Size = new Size(75, 23);
+            btnDelete_Click.TabIndex = 1;
+            btnDelete_Click.Text = "button2";
+            btnDelete_Click.UseVisualStyleBackColor = true;
+            btnDelete_Click.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(32, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // titleBox
+            // 
+            titleBox.Location = new Point(277, 192);
+            titleBox.Name = "titleBox";
+            titleBox.Size = new Size(100, 23);
+            titleBox.TabIndex = 3;
+            // 
+            // authorBox
+            // 
+            authorBox.Location = new Point(277, 234);
+            authorBox.Name = "authorBox";
+            authorBox.Size = new Size(100, 23);
+            authorBox.TabIndex = 4;
+            authorBox.TextChanged += textBox2_TextChanged;
+            // 
+            // yearBox
+            // 
+            yearBox.Location = new Point(282, 303);
+            yearBox.Name = "yearBox";
+            yearBox.Size = new Size(95, 23);
+            yearBox.TabIndex = 5;
+            // 
+            // genreBox
+            // 
+            genreBox.Location = new Point(270, 368);
+            genreBox.Name = "genreBox";
+            genreBox.Size = new Size(100, 23);
+            genreBox.TabIndex = 6;
+            // 
+            // clientBox
+            // 
+            clientBox.Location = new Point(266, 419);
+            clientBox.Name = "clientBox";
+            clientBox.Size = new Size(100, 23);
+            clientBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(clientBox);
+            Controls.Add(genreBox);
+            Controls.Add(yearBox);
+            Controls.Add(authorBox);
+            Controls.Add(titleBox);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnDelete_Click);
+            Controls.Add(btnAdd_Click);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnAdd_Click;
+        private Button btnDelete_Click;
+        private DataGridView dataGridView1;
+        private TextBox titleBox;
+        private TextBox authorBox;
+        private TextBox yearBox;
+        private TextBox genreBox;
+        private TextBox clientBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,128 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
 namespace Library
 {
     public partial class Form1 : Form
     {
+        private const string connectionString = @"Data Source=KOMPUTER;Initial Catalog=Library;Integrated Security=True;";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT ID, Title, Author, Publication_Year, Genre, Client FROM BookJournal";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* using (SqlConnection connection = new SqlConnection(connectionString))
+             {
+                 string query =  $"insert into BookJournal (Title, Author, Publication_Year, Genre, Client) values ({titleBox.Text}, {authorBox.Text}, {yearBox.Text}, {genreBox.Text}, {clientBox.Text})";
+
+                 SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                 try
+                 {
+                     connection.Open();
+                     adapter.SelectCommand.ExecuteNonQuery();
+                     MessageBox.Show("Запись добавлена успешно");
+                 }
+                 catch (Exception ex)
+                 {
+                     MessageBox.Show($"Ошибка при добавлении записи: {ex.Message}");
+                 }
+
+             }*/
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO BookJournal (Title, Author, Publication_Year, Genre, Client) " +
+                               "VALUES (@Title, @Author, @Publication_Year, @Genre, @Client)";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Title", titleBox.Text);
+                command.Parameters.AddWithValue("@Author", authorBox.Text);
+                command.Parameters.AddWithValue("@Publication_Year", yearBox.Text);
+                command.Parameters.AddWithValue("@Genre", genreBox.Text);
+                command.Parameters.AddWithValue("@Client", clientBox.Text);
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Запись добавлена успешно");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Не удалось добавить запись");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при добавлении записи: {ex.Message}");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                int bookId = Convert.ToInt32(dataGridView1.Rows[selectedIndex].Cells["ID"].Value);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM BookJournal WHERE ID = @ID";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@ID", bookId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Please select a book to delete.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
